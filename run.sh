1d6c8cadcbaa9364502ba098c54f978253659bc6
sarialMa <linhai.ma@yale.edu>
2025-10-14 14:31:06 -0400

    eval 2 nd to day

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 RESULT_PATH="/home/lm2445/project_pi_sjf37/lm2445/Bert_PV_classification_1013_New_Sample1-14/samplenew_1_14"
 # bash fine_tune_all.sh topic $RESULT_PATH
-# python eval_all.py -m topic -i "$RESULT_PATH"_topic
+python eval_all.py -m topic -i "$RESULT_PATH"_topic
 # sleep 604800
-bash fine_tune_all.sh pinfo $RESULT_PATH
+# bash fine_tune_all.sh pinfo $RESULT_PATH
 python eval_all.py -m pinfo -i "$RESULT_PATH"_pinfo
 # sleep 604800
 # bash fine_tune_all.sh original $RESULT_PATH
-# python eval_all.py -m original -i "$RESULT_PATH"_original
+python eval_all.py -m original -i "$RESULT_PATH"_original
