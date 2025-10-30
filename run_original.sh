80c613fe6af26dea135dbba9ff243e4ad28a53d6
sarialMa <rinnkai.ba2019@gmail.com>
2025-10-30 16:44:36 -0400

    run for the 1135 version

--- run_original.sh
+++ run_original.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULT_PATH="/home/lm2445/project_pi_sjf37/lm2445/Bert_PV_classification_1013_New_Sample1-14/samplenew_1_14_BWS_remove"
+RESULT_PATH="/home/lm2445/project_pi_sjf37/lm2445/Bert_PV_classification_1013_New_Sample1-14/samplenew_1_14_BWSD"
 # bash fine_tune_all.sh topic $RESULT_PATH
 # python eval_all.py -m topic -i "$RESULT_PATH"_topic
 # sleep 604800
@@ -8,5 +8,5 @@ RESULT_PATH="/home/lm2445/project_pi_sjf37/lm2445/Bert_PV_classification_1013_Ne
 # python eval_all.py -m pinfo -i "$RESULT_PATH"_pinfo
 # sleep 604800
 # maybe can use the method to replace the remove...
-bash fine_tune_all_remove.sh original $RESULT_PATH
-python eval_all_remove.py -m original -i "$RESULT_PATH"_original
+bash fine_tune_all.sh original $RESULT_PATH
+python eval_all.py -m original -i "$RESULT_PATH"_original
